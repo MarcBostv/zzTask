46844e22ade08f77d180be876bed6365e35e15c5
Mathieu BELIN <belinmat@etud.rcisima.isima.fr>
2016-01-12 16:40:38 +0100

    affichage au 12 Janv

--- header.php
+++ header.php
@@ -18,12 +18,6 @@
 					<li <?php if ($selected == "accueil") echo 'class="active"' ?>><a href="accueil.php"><?php echo $lang['ALL_TASKS']; ?></a></li>
 					<li <?php if ($selected == "nouvelleTache") echo 'class="active"' ?>><a href="nouvelleTache.php"><?php echo $lang['CREATE_TASK']; ?></a></li>
 	      		</ul>
-	      		<form class="navbar-form navbar-left" role="search">
-	        		<div class="form-group">
-						<input type="text" class="form-control" placeholder="Search">
-	        		</div>
-	        		<button type="submit" class="btn btn-default">Submit</button>
-	      		</form>
 	      		<ul class="nav navbar-nav navbar-right">
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $lang['LANG'];?><span class="caret"></span></a>
