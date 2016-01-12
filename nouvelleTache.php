1255927e852fade4233303648ae4e736ae96981d
Mathieu BELIN <belinmat@etud.rcisima.isima.fr>
2016-01-12 14:07:10 +0100

    barreNav au 12 Janv

--- nouvelleTache.php
+++ nouvelleTache.php
@@ -21,6 +21,8 @@
   	}
 
 	include_once controleLang();
+	
+	$selected="nouvelleTache";
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,48 +34,8 @@
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     	<script src="bootstrap.js"></script>
 
+		<?php include('header.php');?>
 
-
-		<!-- > Texte d'explication <-->	
-		<nav class="navbar navbar-default">
-	  		<div class="container-fluid">
-    			<!-- Brand and toggle get grouped for better mobile display -->
-    			<div class="navbar-header">
-      				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        				<span class="sr-only">Toggle navigation</span>
-        				<span class="icon-bar"></span>
-        				<span class="icon-bar"></span>
-        				<span class="icon-bar"></span>
-      				</button>
-      				<a class="navbar-brand" href="accueil.php"><?php echo $lang['HEADER_TITLE']; ?></a>
-    			</div>
-
-    			<!-- Collect the nav links, forms, and other content for toggling -->
-    			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      				<ul class="nav navbar-nav">
-       					<li><a href="accueil.php"><?php echo $lang['ALL_TASKS']; ?></a></li>
-        				<li class="active"><a href="nouvelleTache.php"><?php echo $lang['CREATE_TASK']; ?><span class="sr-only">(current)</span></a></li>
-        				</li>
-      				</ul>
-      				<form class="navbar-form navbar-left" role="search">
-        				<div class="form-group">
-          					<input type="text" class="form-control" placeholder="Search">
-        				</div>
-        				<button type="submit" class="btn btn-default">Submit</button>
-      				</form>
-      				<ul class="nav navbar-nav navbar-right">
-      					<li class="dropdown">
-          					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $lang['LANG'];?><span class="caret"></span></a>
-          					<ul class="dropdown-menu">
-                     			<li><a href="nouvelleTache.php?lang=fr">Fr</a></li>
-                      			<li><a href="nouvelleTache.php?lang=en">En</a></li>
-          					</ul>
-        				</li>
-        				<li><a href="accueil.php?action=deco"><?php echo $lang['LOGOUT'];?></a></li>
-      				</ul>
-    			</div><!-- /.navbar-collapse -->
-  			</div><!-- /.container-fluid -->
-		</nav>
 	</head>
 	<body>
 		<form method="post" action="">
